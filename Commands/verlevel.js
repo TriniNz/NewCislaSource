15d848955d4fc7377a644c01efbee9a6efb0846f
Marcos Otavio <contato.ztrininz@hotmail.com>
2020-01-09 15:15:06 -0300

    Ajustes na imagem do comando de levels.

--- Commands/verlevel.js
+++ Commands/verlevel.js
@@ -43,40 +43,41 @@ exports.run = async (Discord, thisClient, message, args, db) => {
         
         let valor = db.get('RankSystem').find({"id": member.id}).value();
 
-        let fotoURL = message.author.displayAvatarURL;
+        let fotoURL = member.user.displayAvatarURL;
 
         let xpDisplay = valor.XPdisplay;
         let xp = valor.XPcount;
         let name = member.user.username;
         let level = valor.Level;
+        let limiteDisplay = formatNumber(level*500)
     
         let limite = level*500
         let ondasys = Math.floor(xp/level*100/550)
     
-        let Background = await Jimp.read('https://cdn.discordapp.com/attachments/560256504998133780/641377423492907008/Background.png')
-        let ondaVerde = await Jimp.read('https://cdn.discordapp.com/attachments/560256504998133780/641377441235075132/poca_verde.png')
-        let firstGround = await Jimp.read('https://cdn.discordapp.com/attachments/560256504998133780/641377437372121088/first_ground.png')
-        let foto = await Jimp.read(fotoURL)
-        let mascara = await Jimp.read('https://cdn.discordapp.com/attachments/560256504998133780/641377439477399553/mask.png')
+        let userphoto = await Jimp.read(fotoURL);
+        let background = await Jimp.read('https://cdn.discordapp.com/attachments/570796258835562496/664734896517939201/Background.png');
+        let bar = await Jimp.read('https://cdn.discordapp.com/attachments/570796258835562496/664734965975744522/bar.png');
+        let firstground = await Jimp.read('https://cdn.discordapp.com/attachments/570796258835562496/664735023425126400/firstground.png');
+        let icon = await Jimp.read('https://cdn.discordapp.com/attachments/570796258835562496/664755763826262053/iconurl.png')
+        let mask = await Jimp.read('https://cdn.discordapp.com/attachments/570796258835562496/664734810006224896/mask.png');
+
+        let font = await Jimp.loadFont('./font/font.fnt');
+
+        mask.resize(760, 760);
+        userphoto.resize(760, 760);
+        icon.resize(240, 240)
+
+        userphoto.mask(mask);
         
-        let font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
-        let fontXP = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
-    
-        mascara.resize(150, 150);
-        foto.resize(150, 150);
-        foto.mask(mascara);
-    
-        Background.composite(ondaVerde, 0, 100-ondasys);
-        Background.composite(firstGround, 0, 0);
-        Background.composite(foto, 49, 32);
-    
-        Background.print(font, 200, 11, name);
-        Background.print(fontXP, 300, 85, xpDisplay);
-        Background.print(fontXP, 355, 140, level).write('image.png');
-    
-        message.channel.send(``, {files: ['image.png']}).then(m => {
-            Fs.unlink('image.png', (err) => {if(err) console.log(err)})
-        })
+
+        background.composite(bar, -2215+ondasys*22, 0);
+        background.composite(firstground, 0, 0);
+        background.composite(userphoto, 112, 442-10)
+        background.composite(icon, 597 + 25, 727 + 210)
+
+        background.print(font, 1066, 750 + 20, name)
+        background.print(font, 1582, 981, ondasys + "%")
+        background.print(font, 1258, 1219, "XP : " + xpDisplay + "/" + limiteDisplay)
 
 
     } catch (err) {
