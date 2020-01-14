1f7693c2ea1b610513b63492be3fb486989f25e1
TriniNz <46603279+TriniNz@users.noreply.github.com>
2020-01-14 20:18:58 -0300

    Update form.js

--- Commands/form.js
+++ Commands/form.js
@@ -1,6 +1,6 @@
 exports.run = async (Discord, thisClient, message, args) => {
     message.channel.send(new Discord.RichEmbed()
-        .setDescription("Para acessar o formulario oficial do Cisla [clique aqui!](https://www.cisla.com.br/)")
+        .setDescription("Para acessar o formulario oficial do Cisla [clique aqui!](https://bit.ly/2knNsH8)")
         .setFooter('CislaSource ©', thisClient.user.displayAvatarURL)
         .setColor('#f83989')
     ).then(msg => {message.delete(15*1000); msg.delete(15*1000)})
