aacff552ee2473443d5bfa2dcd618b81949594a7
Marcos Otavio <contato.ztrininz@hotmail.com>
2020-01-23 14:07:16 -0300

    Arrumando bugs...

--- Package/CislaProtect/Events/ready.js
+++ Package/CislaProtect/Events/ready.js
@@ -1,5 +1,5 @@
 exports.run = async (Discord, client) => {
 
-    setTimeout(() => console.log("CislaProtect - OK! Ping: " + Math.floor(thisClient.ping) + "MS"))
+    setTimeout(() => console.log("CislaProtect - OK! Ping: " + Math.floor(thisClient.ping) + "MS"), 3*1000)
 
 }
--- Package/CislaSource/Events/ready.js
+++ Package/CislaSource/Events/ready.js
@@ -5,7 +5,7 @@ exports.run = async (Discord, thisClient) => {
     const BoleanAutoConsoleMSG = false;
     const BoleanAutoTabbleEdit = true
 
-    setTimeout(() => console.log("CislaSource - OK! Ping: " + Math.floor(thisClient.ping) + "MS"))
+    setTimeout(() => console.log("CislaSource - OK! Ping: " + Math.floor(thisClient.ping) + "MS"), 3*1000)
 
     function AutoEditStats() {
 
