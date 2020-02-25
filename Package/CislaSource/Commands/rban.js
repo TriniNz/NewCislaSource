16fe29b5983cb7b61e5c91aac20f5efd7ad52ea7
TriniNz <46603279+TriniNz@users.noreply.github.com>
2020-02-25 16:19:20 -0300

    Adições e alterações.

---
+++ Package/CislaSource/Commands/rban.js
@@ -0,0 +1,33 @@
+exports.run = async (Discord, thisClient, message, args) => {
+	
+  let client = thisClient
+  
+	if(!message.member.roles.some(r => ["★» Diretor","★» Gerente","★» Administrador","★» Moderador","★» GC","★» Ajudante"].includes(r.name))) return message.channel.send(new Discord.RichEmbed()
+		.setDescription('Você não tem permissão para executar este comando.')
+		.setFooter('CislaSource ©', thisClient.user.displayAvatarURL)
+        .setColor('#f83989')
+	);
+	
+	let string = args.join(' ');
+	let item = string.split(';');
+	
+  const	name = item[0];
+  const motivo = item[1];
+  const prova = item[2];
+		
+	if(!name || !motivo || !prova) return message.channel.send(new Discord.RichEmbed()
+		.setDescription("Comando utilizado incorretamente. O uso correto é `!regban Nickname; Motivo; Prova.`")
+		.setFooter('CislaSource ©', thisClient.user.displayAvatarURL)
+        .setColor('#f83989')
+	)
+	
+	client.channels.get('681934440888533045').send(new Discord.RichEmbed()
+		.setAuthor(message.author.username, message.author.displayAvatarURL)
+		.setDescription("registrou uma punição.")
+		.addField('Usuario punido:', name)
+		.addField('Motivo:', motivo)
+		.addField('Provas:', prova)
+		.setFooter('CislaSource ©', thisClient.user.displayAvatarURL)
+        .setColor('#f83989')
+	).then(Sucess => message.channel.send(new Discord.RichEmbed().setFooter('Punição registrada com sucesso.').setColor('#f83989')));
+}
